5a4a7dec8a9b4740c945c6da09ec6b96f193d49f
Moulya <moulya.anjanappa@gmail.com>
2024-07-16 22:21:31 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ProjectsSidebar from "./components/ProjectsSidebar";
+
+function App() {
+  return (
+    <main className = "sidebar">
+      <ProjectsSidebar/>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/ProjectsSidebar.jsx
@@ -0,0 +1,12 @@
+export default function ProjectsSidebar () {
+    return (
+        <aside className = "funcsidebar">
+            <h2 className="h2style">Your Projects</h2>
+            <div>
+                <button className="btn">+ Add Project</button>
+            </div>
+            <ul></ul>
+        </aside>
+    )
+
+}
