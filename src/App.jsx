cb295cbbe420ac37dc87c22d45ace5eea38e764b
Moulya <moulya.anjanappa@gmail.com>
2024-07-18 22:46:30 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,49 @@
+import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/noProjectSelected";
 import ProjectsSidebar from "./components/ProjectsSidebar";
+import { useState } from "react";
 
 function App() {
+  const [projectsState,setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects:[]
+
+  });
+  function handleStartAddProject () {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId:null,
+      };
+    }
+
+    );
+  }
+  function handleAddProject (projectData) {
+    setProjectsState(prevState => {
+      const newProject = {
+        ...projectData,
+        id: Math.random()
+      };
+      return{
+       ...prevState,
+       projects:[...prevState.projects,newProject]
+      };
+    });
+}
+console.log(projectsState);
+  let content;
+  if(projectsState.selectedProjectId===null){
+    content = <NewProject onAdd = {handleAddProject}/>
+  }
+  else if (projectsState.selectedProjectId===undefined)
+  {
+    content= <NoProjectSelected onStartAddProject = {handleStartAddProject}/>
+  }
   return (
-    <main className = "h-screen my-8" >
-      <ProjectsSidebar/>
+    <main className = "h-screen my-8 flex gap-8" >
+      <ProjectsSidebar onStartAddProject = {handleStartAddProject}/>
+     {content}
     </main>
   );
 }
--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,23 +1,33 @@
-export default function NewProject() {
+import Input from "./Input"
+import { useRef } from "react"
+export default function NewProject({onAdd}) {
+    const title = useRef();
+    const description = useRef();
+    const dueDate = useRef();
+    function handleSave() {
+        const enteredTitle = title.current.value;
+        const enteredDescription = description.current.value;
+        const enteredDuedate = dueDate.current.value;
+
+        onAdd ({
+            title:enteredTitle,
+            description:enteredDescription,
+            dueDate:enteredDuedate,
+
+        })
+    }
+    
     return (
-        <div>
-            <menu>
-                <li><button>Cancel</button></li>
-                <li><button>Save</button></li>
+        <div className="w-[35rem] mt-16">
+            <menu className="flex items-center justify-end gap-4 my-4">
+                <li><button className="text-stone-800 hover:text-stone-950">Cancel</button></li>
+                <li><button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+                onClick = {handleSave}>Save</button></li>
             </menu>
             <div>
-                <p>
-                    <label>Title</label>
-                    <input />
-                </p>
-                <p>
-                    <label>Description</label>
-                    <textarea></textarea>
-                </p>
-                <p>
-                    <label>Due Date</label>
-                    <input />
-                </p>
+               <Input type ="text" ref = {title} label="Title"/>
+               <Input ref = {description} label="Description" textarea/>
+               <Input type = "date" ref = {dueDate} label="Due Date"/>
             </div>
         </div>
     )
